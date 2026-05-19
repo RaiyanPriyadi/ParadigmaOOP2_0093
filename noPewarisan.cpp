5f78f9fdda71190548cb10ab2f27b248b2254357
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-05-19 17:40:44 +0700

    class public

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -8,3 +8,23 @@ public:
     }
 };
 
+class baseClass1 {
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya function dari base class";
+    }
+};
+
+class derivedClass : public baseClass1 {
+public:
+    void perkenalan(){
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+
+int main(){
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
