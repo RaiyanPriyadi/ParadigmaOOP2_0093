dd2be598eba224cf39d53625087aa55f06b99063
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-05-19 17:44:58 +0700

    class lia

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -17,3 +17,31 @@ public:
     }
 };
 
+class joko : public seseorang{
+public:
+    void pesan(){
+    cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia : public seseorang{
+public:
+    void pesan(){
+        cout << "Pesan dari lia" << endl;
+    }
+};
+
+int main (){
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+    //b.seseorang::pesam();
+    
+    return 0;
+}
