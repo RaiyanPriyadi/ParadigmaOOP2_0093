0929e8d3caa5d0425df46cdbc8e481adb6e06966
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-05-19 17:51:13 +0700

    public void

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -4,3 +4,10 @@ using namespace std;
 class AbstraksiKlas {
     private: 
         string x, y;
+    public :
+        //methode untuk mengisi nilai
+        //private nilai
+        void setXY(string a, string b) {
+        x = a;
+        y = b;
+    
